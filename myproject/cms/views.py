321c2cab6cfa78a3c3c9d3b866078de2a2f70971
Pedro Arias Perez <parias@gamma07.aulas.gsyc.urjc.es>
2018-04-25 12:20:26 +0200

    Vista basica para barra creada

--- myproject/cms/views.py
+++ myproject/cms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pages
 
 # Create your views here.
+def barra(request):
+    pages = Pages.objects.all()
+    lista = '<ul>'
+    for page in pages:
+        lista += '<li>' + page.name + '</li>'
+    lista += '</ul>'
+    return HttpResponse(lista)
